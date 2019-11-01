03221ab5137f8ae46e81691da62ee11c7df69c0e
Esteban S <esteban.soubiran@insa-cvl.fr>
2019-11-01 01:15:38 +0100

    add touch for drawer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 v-app
   navbar-app
-  v-content
+  v-content(v-touch="{right: () => setDrawer(true)}")
     v-container(fluid)
       popups-app
       router-view
@@ -12,12 +12,18 @@ v-app
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import Popups from '@/components/Popups'
+import { mapActions } from 'vuex'
 
 export default {
   components: {
     'navbar-app': Navbar,
     'footer-app': Footer,
     'popups-app': Popups
+  },
+  methods: {
+    ...mapActions({
+      setDrawer: 'navbar/drawer'
+    })
   }
 }
 </script>
